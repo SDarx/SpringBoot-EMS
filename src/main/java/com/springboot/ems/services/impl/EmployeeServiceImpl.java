393a3766fe534b1e145571a8ef2d9c0956e001f7
Success John <successojohn@gmail.com>
2023-02-05 21:33:45 +0100

    Get Employee By Id impl

--- src/main/java/com/springboot/ems/services/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/ems/services/impl/EmployeeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.springboot.ems.services.impl;
 
+import com.springboot.ems.customException.ResourceNotFoundException;
 import com.springboot.ems.model.Employee;
 import com.springboot.ems.repository.EmployeeRepository;
 import com.springboot.ems.services.EmployeeService;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 
@@ -20,6 +24,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 
    public List<Employee> GetAllEmployees(){
         return _employeeRepository.findAll();
+   }
+
+   public Employee getEmployeeById(Long Id){
+        return _employeeRepository.findById(Id).orElseThrow(() -> {
+            return new ResourceNotFoundException("Employee","Id",Id);
+        });
    }
 
 }
